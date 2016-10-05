87d84d0e88ec08ff0123a0e020e442f15db9f9de
Denis Levkov <denisl@nana10.tv>
2016-10-05 18:28:43 +0300

    Added service

--- src/app/common/Constants.ts
+++ src/app/common/Constants.ts
@@ -1,4 +1,7 @@
 export class Constants {
     public static readonly SERVICE_KEY: string = 'service';
     public static readonly SEARCH_KEY: string = 'search';
+    public static readonly IMAGE_LOADING_URL = '../../assets/img/preload640-640.png';
+    public static readonly IMAGE_LOADING_URL16_9 = '../../assets/img/preload_640-360.png';
+    public static readonly DATA_DOMAIN = 'http://localhost/Nana10MVC/';
 }

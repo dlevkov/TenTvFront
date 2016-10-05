eb58c7ba0305a496f88b4a8997424c3df2a57edd
Denis Levkov <denisl@nana10.tv>
2016-10-05 16:23:07 +0300

    fill targeted folder

--- src/app/common/Enums.ts
+++ src/app/common/Enums.ts
@@ -17,3 +17,12 @@ export enum ImageTypes {
     Medium,
     Big
 }
+export enum HeadlineItemTypes {
+    Big = 10,
+    Video = 1,  // temporary replaced by standard item in views
+    Main = 2,
+    Ad = 8,
+    Pair = 9,
+    Small = 0,
+    Alert = 6
+}

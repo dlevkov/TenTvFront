3dc3a70738bd57c8e864a398824153c02a844dbf
Mark Soifer <posurlu@gmail.com>
2016-10-09 17:33:21 +0300

    Headline folder's name fix

---
+++ src/app/common/components/headlines/headline-main.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { HeadlineModel } from '../../models/headline.model';
+@Component({
+    selector: 'headline-main',
+    templateUrl: 'headline-main.component.html'
+})
+export class HeadlineMainComponent implements OnInit {
+    @Input() item: HeadlineModel;
+    constructor() { }
+ 
+    ngOnInit() { }
+}
+ 
+ 

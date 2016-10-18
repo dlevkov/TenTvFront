37382d176461a5e65b4eca3c6689bfac7f5c186a
Mark Soifer <posurlu@gmail.com>
2016-10-18 09:36:50 +0300

    main component moved to "main folder" default route changed to "main component"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,17 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NoContent } from './common/components/no-content';
 import { SectionComponent } from './targeted/components/section/section.component';
-import { MainComponent } from './targeted/components/main.component';
+import { MainComponent } from './targeted/components/main/main.component';
 import { DataResolver } from './app.resolver';
 import { ArticleComponent } from './targeted/components/article/article.component';
 import { TwitterComponent } from './common/components/twitter/twitter.component';
 
 
 export const ROUTES: Routes = [
-  { path: '', component: NoContent },
+  { path: '', component: MainComponent },
+  { path: 'main', component: MainComponent, },
   { path: 'section/:id', component: SectionComponent },
   { path: 'article/:id', component: ArticleComponent },
-  { path: 'main', component: MainComponent },
   { path: 'twitter', component: TwitterComponent },
   { path: '**', component: NoContent },
 

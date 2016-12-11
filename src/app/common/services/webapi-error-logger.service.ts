9e4050273b6c402d6e7f983c8e709d6ded076e0f
Denis Levkov <denisl@nana10.tv>
2016-12-11 15:59:05 +0200

    custom error handler on

--- src/app/common/services/webapi-error-logger.service.ts
+++ src/app/common/services/webapi-error-logger.service.ts
@@ -1,22 +1,34 @@
-import { Injectable } from '@angular/core';
-import { Http, Headers, Response } from '@angular/http';
-import { Observable } from 'rxjs/Rx';
+import { Injectable, OnDestroy } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable, Subscription } from 'rxjs/Rx';
 import { Dal } from '../services/dal.service';
-import { GtmModel } from '../models/gtm.model';
-
+import { Constants } from '../Constants';
 
 @Injectable()
-export class WebApiErrorLogger {
+export class WebApiErrorLogger implements OnDestroy {
     private _dal: Dal;
-
+    private _dataDomain: string = Constants.DATA_DOMAIN;
+    private _subscriber: Subscription;
     constructor(private _http: Http) {
         this._dal = new Dal(_http);
     }
 
     Log(title: string, message: any): boolean {
-        this._http.post('Logger/log', { title, message })
-            .catch(this._dal.handleError);
+        // this._http.get('http://localhost/Nana10MVC/TenTvAppFront/errorlog?ErrorDesc=112235')
+        if (!this._subscriber)
+            this._subscriber = this.sendMessage(message).subscribe();
         return false;
+    }
+    sendMessage(message: string): Observable<any> {
+        return this._http.get(this._dataDomain + 'TenTvAppFront/errorlog?ErrorDesc=' + message)
+            .map((res) => {
+                return true;
+            })
+            ;
+    }
+
+    ngOnDestroy() {
+        this._subscriber.unsubscribe();
     }
 }
 
